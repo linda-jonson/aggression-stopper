388aa7ed70bf7d607bca692f82e631466bd33684
linda.jonson <>
2022-03-03 14:45:13 +0200

    Added Docker support.

---
+++ distrib/run-in-container.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+CURRENT_PATH=`pwd`
+
+JAR_TO_RUN=`ls aggression-stopper-*.jar`
+
+/opt/openjdk-17/bin/java -server -Xms128m -Xmx256m -XX:+UseContainerSupport -Djava.net.preferIPv4Stack=true -Djava.awt.headless=true --add-opens java.base/jdk.internal.misc=ALL-UNNAMED -Dio.netty.tryReflectionSetAccessible=true -jar "$JAR_TO_RUN" --spring.config.location=application.yml
